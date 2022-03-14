7901244ec4c391a0bc60f9de3ae1777d4d7e6ac6
Neelpatel5998 <101489560+Neelpatel5998@users.noreply.github.com>
2022-03-14 10:39:52 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+$userLoggedIn = $_SESSION["userLoggedIn"];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Neelflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+      
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"></script>
+        <script src="assets/js/script.js"></script>
+    </head>
+  
