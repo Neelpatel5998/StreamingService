75209f3c48ae7329dcdd65e2b803b33af5b6699b
Neelpatel5998 <101489560+Neelpatel5998@users.noreply.github.com>
2022-04-09 11:08:41 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
   <head>
     <title>Welcome to Reeceflix</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+  </head>
 <?php
   <body>
     hotdogs
