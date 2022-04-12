bc4d4524d8ec480508d9dacfc1e8a32a1b056606
Neelpatel5998 <101489560+Neelpatel5998@users.noreply.github.com>
2022-04-12 00:42:39 -0400

    Update register.php

--- register.php
+++ register.php
@@ -7,7 +7,22 @@
     <div class="SignInContainer">
       <div class="column">
         <form method="POST">
-          <input type="text" >
+          <input type="text" name="firstName" placeholder="First name">
+          
+          <input type="text" name="lastName" placeholder="Last name">
+          
+          <input type="text" name="username" placeholder="Username">
+          
+          <input type="email" name="email" placeholder="Email">
+          
+          <input type="email" name="email2" placeholder="Confirm email">
+          
+          <input type="password" name="password" placeholder="Password">
+          
+          <input type="password" name="password2" placeholder="Confirm password">
+          
+          <input type="submit" name="submitPassword" value="SUBMIT">
+         
         </form>
       </div>
     </div>
