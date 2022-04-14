a09927d6f1304003fe16259aa495738fd7f0202f
Neelpatel5998 <101489560+Neelpatel5998@users.noreply.github.com>
2022-04-14 15:33:44 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,8 @@
+<?php
+    if(isset($_POST["submitButton"])) {
+        echo "Form was submitted";
+    }  
+?>
 <!DOCTYPE html>
   <head>
     <title>Welcome to Reeceflix</title>
@@ -7,21 +12,21 @@
     <div class="SignInContainer">
       <div class="column">
         <form method="POST">
-          <input type="text" name="firstName" placeholder="First name">
+          <input type="text" name="firstName" placeholder="First name" required>
           
-          <input type="text" name="lastName" placeholder="Last name">
+          <input type="text" name="lastName" placeholder="Last name" required>
           
-          <input type="text" name="username" placeholder="Username">
+          <input type="text" name="username" placeholder="Username" required>
           
-          <input type="email" name="email" placeholder="Email">
+          <input type="email" name="email" placeholder="Email" required>
           
-          <input type="email" name="email2" placeholder="Confirm email">
+          <input type="email" name="email2" placeholder="Confirm email" required>
           
-          <input type="password" name="password" placeholder="Password">
+          <input type="password" name="password" placeholder="Password" required>
           
-          <input type="password" name="password2" placeholder="Confirm password">
+          <input type="password" name="password2" placeholder="Confirm password" required>
           
-          <input type="submit" name="submitPassword" value="SUBMIT">
+          <input type="submit" name="submitButton" value="SUBMIT">
          
         </form>
       </div>
