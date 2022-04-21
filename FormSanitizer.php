6bd53677b63b4eda50aac5641c2dd5f4240536d7
Neelpatel5998 <101489560+Neelpatel5998@users.noreply.github.com>
2022-04-20 21:02:05 -0400

    Update FormSanitizer.php

--- FormSanitizer.php
+++ FormSanitizer.php
@@ -1,6 +1,6 @@
 <?php
 class FormSanitizer {
-  
+   
   public static function sanitizeFormString($inputText) {
     $inputText = strip_tags($inputText);
     $inputText = str_replace(" ", "", $inputText);
