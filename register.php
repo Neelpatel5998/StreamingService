61f77cd73a4cc83889228adcef47eaddebf21acb
Neelpatel5998 <101489560+Neelpatel5998@users.noreply.github.com>
2022-05-16 18:34:32 -0400

    Update register.php

--- register.php
+++ register.php
@@ -16,7 +16,7 @@ require_once("includes/classes/Account.php");
         $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
         $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
         
-        $account->register($firstName, $lastName, $username, $email)
+        $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
     }  
 ?>
 <!DOCTYPE html>
